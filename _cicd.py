446109b7724215c7e056ced6dbe0d09a1599fb2b
Patrick Veltrop <43636101+43874@users.noreply.github.com>
2020-06-05 00:57:04 +0200

    Update _cicd.py

--- _cicd.py
+++ _cicd.py
@@ -81,6 +81,7 @@ def TestSucceeded():
             
 # Prepare Laravel
 try:
+    print('foo')
     os.system('cp .env.testing .env')
     os.system('php artisan key:generate')
     os.system('php artisan config:clear')
