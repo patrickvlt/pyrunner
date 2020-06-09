bc25d5c55d905a8e8344ad3c8bc271cedd45b5d0
Patrick Veltrop <patrick@jottenheijm.com>
2020-06-09 16:08:32 +0200

    Update

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import requests
-open('_init.py', 'wb').write(requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_init.py').content)
-open('_cicd.py', 'wb').write(requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_cicd.py').content)
+open('vendor/pveltrop/pyrunner/_init.py', 'wb').write(requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_init.py').content)
+open('vendor/pveltrop/pyrunner/_cicd.py', 'wb').write(requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_cicd.py').content)
 
 import _init as pr
 import _tests as tests
