1d9adee1257abcffeb2a18eed9a2bfd9da2cf73b
Patrick <patrick@jottenheijm.com>
2020-06-09 18:47:27 +0200

    Update

--- repo.py
+++ repo.py
@@ -2,6 +2,6 @@ import os
 
 os.system('git add .')
 os.system('git commit -m "Update"')
-os.system('git tag -f v1.9')
+os.system('git tag -f v2.0')
 os.system('git push')
 os.system('git push -f --tags')
