60ee01f408de75ca79a90630b5e27c382a1ecda1
Patrick Veltrop <43636101+43874@users.noreply.github.com>
2020-06-11 13:25:03 +0200

    Create _dbexport.py

---
+++ _dbexport.py
@@ -0,0 +1,50 @@
+import mysql.connector
+import re
+import sys
+import os
+
+customArgs = []
+customArgs.append('--table=')
+
+tableName = None
+
+for customArg in customArgs:
+    for sysArg in sys.argv:
+        sysArg = sysArg.split('=')
+        cmd = sysArg[0]
+        try:
+            val = sysArg[1]
+        except:
+            continue
+        if cmd in customArg:
+            if cmd == '--table':
+                tableName = val
+
+# -----------------------------------------------------------
+# Retrieving data
+# -----------------------------------------------------------
+
+def FindString(key,content):
+    regex = r""+key+"=.*"
+    match = re.search(regex, content)
+    match = match.group()
+    match = match.replace(key+'=','')
+    return match
+
+# Get DB credentials from .env
+f = open(('.env'), 'r')
+env = f.read()
+DB_HOST = FindString("DB_HOST",env)
+DB_PORT = FindString("DB_PORT",env)
+DB_DATABASE = FindString("DB_DATABASE",env)
+DB_USERNAME = FindString("DB_USERNAME",env)
+DB_PASSWORD = FindString("DB_PASSWORD",env)
+
+# Connect to server
+cnx = mysql.connector.connect(
+    host=DB_HOST,
+    port=DB_PORT,
+    user=DB_USERNAME,
+    password=DB_PASSWORD)
+
+os.system("mysqldump -u "+DB_USERNAME+" --password="+DB_PASSWORD+" "+DB_DATABASE+" > database.sql")
