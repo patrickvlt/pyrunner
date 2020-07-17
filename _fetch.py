26a4e591a4fb13e5fa3b5ce05e825e49527a669f
Patrick Veltrop <43636101+43874@users.noreply.github.com>
2020-07-17 16:32:19 +0200

    Update _fetch.py

--- _fetch.py
+++ _fetch.py
@@ -3,43 +3,52 @@ import requests
 import os
 
 print("foo")
-
+    
 runnerContent = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_runner.py').content
-if '404' not in runnerContent:
+runnerStatus = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_runner.py').status_code
+if runnerStatus != 404:
     open('vendor/pveltrop/pyrunner/_runner.py', 'wb').write(runnerContent)
     
 dbContent = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_db.py').content
-if '404' not in dbContent:
+dbStatus = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_db.py').status_code
+if dbStatus != 404:
     open('vendor/pveltrop/pyrunner/_db.py', 'wb').write(dbContent)
     
 initContent = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_init.py').content
-if '404' not in initContent:
+initStatus = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_init.py').status_code
+if initStatus != 404:
     open('vendor/pveltrop/pyrunner/_init.py', 'wb').write(initContent)
     
 cicdContent = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_cicd.py').content
-if '404' not in cicdContent:
+cicdStatus = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_cicd.py').status_code
+if cicdStatus != 404:
     open('vendor/pveltrop/pyrunner/_cicd.py', 'wb').write(cicdContent)
     
 updateContent = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_update.py').content
-if '404' not in updateContent:
+updateStatus = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_update.py').status_code
+if updateStatus != 404:
     open('vendor/pveltrop/pyrunner/_update.py', 'wb').write(updateContent)
     
 # Service Provider
 
-providerContent = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/src/PyRunnerProvider.php').content
-if '404' not in providerContent:
-    open('vendor/pveltrop/pyrunner/src/PyRunnerProvider.php', 'wb').write(providerContent)
+providerContent = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_provider.py').content
+providerStatus = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_provider.py').status_code
+if providerStatus != 404:
+    open('vendor/pveltrop/pyrunner/_provider.py', 'wb').write(providerContent)
     
 # Commands
     
-installCmdContent = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/src/app/Commands/Install.php').content
-if '404' not in installCmdContent:
-    open('vendor/pveltrop/pyrunner/src/app/Commands/Install.php', 'wb').write(installCmdContent)
+InstallContent = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/src/app/Commands/Install.php').content
+InstallStatus = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/src/app/Commands/Install.php').status_code
+if InstallStatus != 404:
+    open('vendor/pveltrop/pyrunner/src/app/Commands/Install.php', 'wb').write(InstallContent)
     
-startCmdContent = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/src/app/Commands/Start.php').content
-if '404' not in startCmdContent:
-    open('vendor/pveltrop/pyrunner/src/app/Commands/Start.php', 'wb').write(startCmdContent)
+StartContent = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/src/app/Commands/Start.php').content
+StartStatus = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/src/app/Commands/Start.php').status_code
+if StartStatus != 404:
+    open('vendor/pveltrop/pyrunner/src/app/Commands/Start.php', 'wb').write(StartContent)
     
-updateCmdContent = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/src/app/Commands/Update.php').content
-if '404' not in updateCmdContent:
-    open('vendor/pveltrop/pyrunner/src/app/Commands/Update.php', 'wb').write(updateCmdContent)
+UpdateContent = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/src/app/Commands/Update.php').content
+UpdateStatus = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/src/app/Commands/Update.php').status_code
+if UpdateStatus != 404:
+    open('vendor/pveltrop/pyrunner/src/app/Commands/Update.php', 'wb').write(UpdateContent)
