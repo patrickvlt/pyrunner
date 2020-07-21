5eb9f70ef748cf03b789ce5adbace3445de613e6
Patrick Veltrop <43636101+43874@users.noreply.github.com>
2020-07-21 12:51:03 +0200

    Update _update.py

--- _update.py
+++ _update.py
@@ -2,7 +2,7 @@
 import requests
 
 fetchContent = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_fetch.py').content
-fetchStatus = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_fetch2py').status_code
+fetchStatus = requests.get('https://raw.githubusercontent.com/43874/pyrunner/master/_fetch.py').status_code
 if fetchStatus != 404:
     open('vendor/pveltrop/pyrunner/_fetch.py', 'wb').write(fetchContent)
     import _fetch
