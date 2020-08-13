89633da06d643e386fb6f09ef76561a8d83f4e76
Patrick <patrickveltrop@live.nl>
2020-08-13 15:20:25 +0200

    Fixed argument typo

--- _cicd.py
+++ _cicd.py
@@ -99,7 +99,7 @@ except Exception as e:
     TestFailed()
 
 # Run python file which runs defined test functions
-exit_code = os.system("php artisan serve --port=80 --env=testing --host=localhost & python vendor/pveltrop/pyrunner/test_app.py debug shell")
+exit_code = os.system("php artisan serve --port=80 --env=testing --host=localhost & python vendor/pveltrop/pyrunner/test_app.py --debug --shell")
     
 if exit_code > 0:
     TestFailed()
